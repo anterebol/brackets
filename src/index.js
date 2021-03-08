89b5fafc4161474af8173f6ae8001ddc32eeba42
unknown <anterebol@gmail.com>
2021-03-08 20:56:31 +0300

    fead: chenge index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let value;
+  for (let k = 0; str.length != 0 && k < 100; k++) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+   value = str.split(bracketsConfig[i].join(''));
+   value = value.join('');
+    if (value.length < str.length) {
+      str = value;
+    }
+    }
+  }
+  if (str.length == 0){
+    return true;
+  }
+    else {
+      return false;
+    }
+  }
+  
